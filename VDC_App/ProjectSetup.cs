769efcc6389c8cb499d526545ae9e4ceec7c1ffa
tdang <tdang@cannistraro.com>
2024-08-08 07:54:28 -0400

    CREATE A SINGULAR LIST THAT INCLUDES ALL LEVEL PARAMS
    Created a list that contain level name, elevation, view range between levels
    and the elementId of the level. Binded params to display in the UI.

--- VDC_App/ProjectSetup.cs
+++ VDC_App/ProjectSetup.cs
@@ -34,6 +34,77 @@ namespace VDC_App
                 .Cast<View>()
                 .Where(x => x.Name.Equals("LEVEL x0001"));
 
+            // get list of levels in project
+            var levels = new collector(doc).GetLevelsList();
+
+            // create the WPF object
+            var ui = new ProjectSetupUI();
+
+            var tempOrdered = levels.OrderBy(e => e.Elevation).ToList();
+            var tempLevElev = new List<LevelsElevations>();
+
+            for (int i = 0; i < tempOrdered.Count; i++)
+            {
+                var test = 0.0;
+                if (i == tempOrdered.Count - 1)
+                {
+                    test = 50.0;
+                    
+                }
+                else
+                {
+                    test = tempOrdered[i + 1].Elevation - tempOrdered[i].Elevation;
+
+                }
+                tempLevElev.Add(new LevelsElevations(tempOrdered[i].Name, tempOrdered[i].Elevation, test, tempOrdered[i].Id));
+            }
+
+            //foreach (var e in tempOrdered)
+            //{
+            //    //TaskDialog.Show("sdsa", e.Elevation.ToString());
+            //    tempLevElev.Add(new LevelsElevations(e.Name, e.Elevation));
+
+
+            //}
+
+
+            //var groupByElevation = tempLevElev
+            //    .OrderBy(e => e.Elevation)
+            //    .ToList();
+
+            //var viewRangeCal = groupByElevation
+            //    .Select(e => e.Elevation)
+            //    .ToList();
+
+
+            //var sd = new List<LevelsElevations>();
+
+            //for (var i = 0; i < viewRangeCal.Count; i++)
+            //{
+            //    if (i == viewRangeCal.Count - 1)
+            //    {
+            //        continue;
+            //    }
+
+            //    var test = viewRangeCal[i + 1] - viewRangeCal[i];
+
+            //    sd.Add(new LevelsElevations(test));
+            //    //sd.Add(test);
+
+            //}
+
+
+
+            foreach (var e in tempLevElev)
+            {
+
+                ui.Levels.Items.Add(new { levelUi = e.Level, elevationUi = e.Elevation, viewRangeUi = e.ViewRange });
+            }
+
+
+            var form = new SimpleForm(tempLevElev);
+            form.ShowDialog();
+            ui.Show();
 
             //var test = Level.GetNearestLevelId(doc, 19.5);
             using (Transaction t = new Transaction(doc))
@@ -55,17 +126,67 @@ namespace VDC_App
 
                 t.Commit();
             }
-            var ui = new ProjectSetupUI();
-
-            foreach (var e in viewTest)
-            {
-                ui.Levels.Items.Add(new { Level = e.Name });
-            }
 
 
-            //ui.Show();
 
             return Result.Succeeded;
         }
     }
 }
+
+public class LevelsElevations
+{
+    public string Level { get; set; }
+    public double Elevation { get; set; }
+    public double ViewRange { get; set; }
+    public ElementId Id { get; set; }
+
+
+    public LevelsElevations(string level, double elevation, double viewRange, ElementId id)
+    {
+        Level = level;
+        Elevation = elevation;
+        ViewRange = viewRange;
+        Id = id;
+    }
+}
+
+public class collector
+{
+    public Document Doc { get; set; }
+    public string ViewName { get; set; }
+
+    public collector(Document doc, string viewName)
+    {
+        Doc = doc;
+        ViewName = viewName;
+    }
+
+    public collector(Document doc)
+    {
+        Doc = doc;
+    }
+
+    public List<View> GetViewsList()
+    {
+        var colList = new FilteredElementCollector(Doc)
+            .OfClass(typeof(View))
+            .Cast<View>()
+            .Where(e => e.Name.ToLower().Contains(ViewName))
+            .ToList();
+            
+        return colList;
+    }
+
+    public List<Level> GetLevelsList()
+    {
+        var levelsCol = new FilteredElementCollector(Doc)
+            .OfClass(typeof(Level))
+            .Cast<Level>()
+            .Where(e => e.get_Parameter(BuiltInParameter.ELEM_PARTITION_PARAM).AsValueString().ToLower().Contains("jcc-levels"))
+            .SkipWhile(e => e.Name.ToLower().Contains("insertion level"))
+            .ToList();
+        return levelsCol;
+    }
+}
+
